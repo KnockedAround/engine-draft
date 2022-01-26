c0d69ad804753da3efdfe576c77e1b8dff414bda
eliaugaudas <evaldrck@gmail.com>
2022-01-25 22:09:45 -0600

    tslib update, removed dupe identifiers, using ngrx schematics as default cli schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   declarations: [
